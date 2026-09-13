73d0ee0495b2b9794a6f25116474c8f0b8794f0f
JacobJohansson <jacobknuts@gmail.com>
2026-09-13 15:58:05 +0200

    Lägg till GitHub Pages-deploy, custom domain samt sticky/ställbar navigering

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // Kopiera statiska filer (bilder, pdf:er) och CSS till bygget
   eleventyConfig.addPassthroughCopy({ "src/static": "static" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
 
   // Datumformatering för nyheter m.m.
   eleventyConfig.addFilter("shortDate", (d) => {
